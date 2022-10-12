98283e768161c18a00ae204cedb6a2d41223d962
sanketnighot <sanketnighot25@gmail.com>
2022-10-12 19:01:09 +0530

    w

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,5 +2,5 @@
 export const NETWORK = 'ghostnet';
 export const RPC = 'https://rpc.ghostnet.teztnets.xyz/';
 export const PRECISION = 1000000000000000000;
-export const CONTRACT_ADDRESS = "KT1Enz6Bv613eLZeykK92cXJ3iyZUWYxEap7";
+export const CONTRACT_ADDRESS = "KT1RqczWL9NqKkeF48eRiRav41YqGtDfsm9a";
 export const vUSD_ADDRESS = 'KT1D5xQy9x7YSgrzTzLJx9tEQ6qK9pSW2vfz';
